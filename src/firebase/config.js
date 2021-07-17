944e463ab83086a892b80efced4d7e4a8824abce
wijaya014 <wijaya.agus014@gmail.com>
2021-07-17 14:56:46 +0700

    add firebase database

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyB6rq-yJrwj28uvMMqSs8nSC8o7pI-I6C4",
+    authDomain: "news-app-9b1fc.firebaseapp.com",
+    projectId: "news-app-9b1fc",
+    storageBucket: "news-app-9b1fc.appspot.com",
+    messagingSenderId: "398544486172",
+    appId: "1:398544486172:web:ca6d70669cb1a0e0a344d9",
+    measurementId: "G-5W0LESWN6W"
+  };
+  
+  //firebase.initializeApp(firebaseConfig);
+
+  export default firebaseConfig;
